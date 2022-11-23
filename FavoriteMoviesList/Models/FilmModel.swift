7086390bfd0b62ea79441b3e41e8e957a71e38be
Aliaksandr96 <Aliaksandr.Hunko@gmail.com>
2022-11-23 16:42:37 +0100

    add film model

---
+++ FavoriteMoviesList/Models/FilmModel.swift
@@ -0,0 +1,11 @@
+import Foundation
+import UIKit
+
+struct FilmModel {
+    let releaseDate: String
+    let imageFilm: Data
+    let name: String
+    let ratings: String
+    let link: String
+    let discription: String
+}
